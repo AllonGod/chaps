80000c5e185a2b4ee86d92405be349221c925170
AllonGod <83020756+AllonGod@users.noreply.github.com>
2026-01-20 07:18:58 +0100

    Create redirect.ts

---
+++ redirect.ts
@@ -0,0 +1,18 @@
+// redirect.ts
+
+// Define the redirect URL
+const redirectUrl = "https://sociable-version-138624.framer.app/
+
+soci"; // Replace with the URL you want to redirect to
+
+// Handle the request
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    new Response(null, {
+      status: 302, // HTTP status for redirection
+      headers: {
+        "Location": redirectUrl, // The URL to redirect to
+      },
+    })
+  );
+});
